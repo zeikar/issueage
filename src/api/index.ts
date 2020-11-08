d643525c52ab570df1a0de3a4cc30b2528120eea
Zeikar <antstp@naver.com>
2020-11-08 17:39:14 +0900

    Delete unnecessary comments

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,5 @@
-// pokemon.js
-// Implementations for all the calls for the pokemon endpoints.
 import Api from "./request";
 
-// Method to get a list of all Pokemon
 export const getAllIssues = async () => {
   try {
     const response = await Api.getAllIssues();
--- src/api/request.ts
+++ src/api/request.ts
@@ -1,13 +1,10 @@
-// Api.js
 import axios from "axios";
 import { GET_ALL_ISSUES } from "./queries";
 
-// Create a instance of axios to use the same base url.
 const axiosAPI = axios.create({
   baseURL: "https://api.github.com/graphql",
 });
 
-// implement a method to execute all the request from here.
 const graphQLRequest = (query: string) => {
   const headers = {
     Authorization: "bearer c2506ee733b85d9c52e9c04164602ba1dee2094f",
@@ -28,7 +25,6 @@ const graphQLRequest = (query: string) => {
 
 const getAllIssues = () => graphQLRequest(GET_ALL_ISSUES);
 
-// expose your method to other services or actions
 const API = {
   getAllIssues,
 };
