e53b482fbefd0644757271831628ded1384c1eba
Zeikar <zeikar@naver.com>
2021-02-11 01:22:01 +0900

    Fix navbar links

--- src/components/common/Navbar.svelte
+++ src/components/common/Navbar.svelte
@@ -4,12 +4,12 @@
 
 <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
-    <a href="/" class="navbar-item">{websiteTitle}</a>
+    <a href="." class="navbar-item">{websiteTitle}</a>
   </div>
 
   <div class="navbar-menu">
     <div class="navbar-start">
-      <a href="/" class="navbar-item"> menu here </a>
+      <a href="." class="navbar-item"> menu here </a>
     </div>
 
     <div class="navbar-end">
